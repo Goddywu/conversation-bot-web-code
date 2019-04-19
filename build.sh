762a68d89d8562db660e828a2a3e1e1b3aae674a
Goddy <wucs@ruyi.ai>
2019-04-19 18:45:05 +0800

    init

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
+set -e
 npm run build
+cd dist
+git init
+git remote add origin https://github.com/Goddywu/conversation-bot.git
 git add .
-git commit -m 'goddy comment'
+git commit -m 'init'
+git push --force -u origin master 
+cd -
+git add .
+git commit -m 'init'
 git push
